fa047220543557a212b01c3fd911f60314abd7fd
Anmoltiwari1 <124855487+Anmoltiwari1@users.noreply.github.com>
2026-02-19 12:06:21 +0530

    [Anmol Tiwari]: add . UC3

---
+++ src/main/java/com/example/UC3_GenericQuantityClassForDRYPrinciple/Uc3GenericQuantityClassForDryPrincipleApplication.java
@@ -0,0 +1,13 @@
+package com.example.UC3_GenericQuantityClassForDRYPrinciple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uc3GenericQuantityClassForDryPrincipleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uc3GenericQuantityClassForDryPrincipleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/UC3_GenericQuantityClassForDRYPrinciple/Uc3GenericQuantityClassForDryPrincipleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UC3_GenericQuantityClassForDRYPrinciple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uc3GenericQuantityClassForDryPrincipleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
