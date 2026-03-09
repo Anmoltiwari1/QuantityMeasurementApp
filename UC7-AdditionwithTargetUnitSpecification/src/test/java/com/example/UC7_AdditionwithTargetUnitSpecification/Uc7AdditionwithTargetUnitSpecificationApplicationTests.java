d5734d06e46d6a45f0fa317fdacdc61a4ad15d83
Anmoltiwari1 <124855487+Anmoltiwari1@users.noreply.github.com>
2026-03-09 15:51:01 +0530

    [Anmol Tiwari]:add . UC7

---
+++ UC7-AdditionwithTargetUnitSpecification/src/test/java/com/example/UC7_AdditionwithTargetUnitSpecification/Uc7AdditionwithTargetUnitSpecificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UC7_AdditionwithTargetUnitSpecification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uc7AdditionwithTargetUnitSpecificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
